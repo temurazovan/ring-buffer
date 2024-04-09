6341eba19ccb5597542e0349394dc42a09d860d5
temurazovan <nadezhdadieslast@gmail.com>
2024-04-10 01:51:12 +0400

    added second branch

--- main.cpp
+++ main.cpp
@@ -8,18 +8,27 @@ void printVector(std::vector<int> vector) {
 }
 
 int main() {
-    std::vector<int> vec(20);
+    std::vector<int> vec;
     int value = 0;
 
     std::cout << "Input up to 20 elements: ";
-    for (int i = 0; i < vec.size(); i++) {
+
+    std::cin >> value;
+    while (value != -1) {
+        if (vec.size() < 20) {
+            vec.push_back(value);
+        } else {
+            for (int i = 0; i < 20; i++) {
+                vec[i] = vec[i + 1];
+                vec[vec.size() - 1] = value;
+            }
+        }
+        while (vec.size() > 20){
+            vec.pop_back();
+        }
         std::cin >> value;
-        if (value != -1) {
-            vec[i] = value;
-        }
-        if (value == -1) {
-            printVector(vec);
-        }
     }
+
+    printVector(vec);
 
 }
